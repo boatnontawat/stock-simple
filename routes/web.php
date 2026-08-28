cf85f2f7bc4d5e948149bb59559209b25a478006
boatnontawat <55104302+boatnontawat@users.noreply.github.com>
2026-08-28 21:18:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/stock-items');
 
+Route::post('stock-items/{stock_item}/use', [StockItemController::class, 'recordUsage'])->name('stock-items.use');
 Route::resource('stock-items', StockItemController::class);
 
 // ถ้าต้องการจำกัดให้ต้อง login ก่อนใช้งาน ให้ครอบด้วย middleware('auth') เช่น:
